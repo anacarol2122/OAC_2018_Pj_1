662b872d4505a4a619672ce185d56b51e1dbbbcd
anacarol2122 <43650635+anacarol2122@users.noreply.github.com>
2018-09-27 16:45:24 -0300

    Update showimg.asm

--- showimg.asm
+++ showimg.asm
@@ -25,17 +25,17 @@
 	
 	
 	lw $s1, 0($a1) # Tamanho do arquivo
-	lw $s2, 8($a1) # Offset para come√ßo dos dados da imagem
+	lw $s2, 8($a1) # Offset para come√É¬ßo dos dados da imagem
 	lw $s3, 16($a1) # Largura da imagem em pixels
 	lw $s4, 20($a1) # Altura da imagem em pixels 
 
 	move $t0, $zero
-	li $t1, 0x10040000 #Prepara o endere√ßo da mem√≥ria heap para armazenar a imagem
+	li $t1, 0x10040000 #Prepara o endere√É¬ßo da mem√É¬≥ria heap para armazenar a imagem
 	
 	show_original_image:
 	
 	beq $t0, $s1, exit_original_image
-	sb $zero, 0($t1)
+	sb $zero, 3($t1)
 	li $a2, 3
 	la $a1, men1
 	jal read_file
@@ -58,7 +58,7 @@
 	 
 	
 	
-	open_file: #Funcao abre arquivo que recebe como argumento em $a0 o endere√ßo onde esta armazenado o 
+	open_file: #Funcao abre arquivo que recebe como argumento em $a0 o endere√É¬ßo onde esta armazenado o 
 	# nome do arquivo e em $a1 a flag (0 - read, 1-write) e retorna em $v0 0 descritor do arquivo.
 	
 	li $v0, 13
@@ -67,21 +67,21 @@
 	jr $ra
 	
 ###################################################################
-# vira a imagem para deixar ela na forma de visualizaÁ„o correta 
+# vira a imagem para deixar ela na forma de visualiza√ß√£o correta 
 close_file: #Funcao fecha arquivo que recebe como argumento em $a0 o descritor do arquivo.
 la $t0,0x10040000
-addi $t1,$s3,-1 # sendo $s3 o uma das dimensıes do arquivo 
-mul $t1,$t1,4  # sendo multiplicado para achar o vavor do endereÁo correto
-mul $t1,$t1,$s4 # sendo $s4 a altra dimens„o do aquivo
-add $t1,$t1,$t0 # somando com o endereÁo base para encontrar o endereÁo correto
+addi $t1,$s3,-1 # sendo $s3 o uma das dimens√µes do arquivo 
+mul $t1,$t1,4  # sendo multiplicado para achar o vavor do endere√ßo correto
+mul $t1,$t1,$s4 # sendo $s4 a altra dimens√£o do aquivo
+add $t1,$t1,$t0 # somando com o endere√ßo base para encontrar o endere√ßo correto
 div $t2,$s4,2 # dividindo a altura por dois para encontrar o ponto de parada 
 
 loop2: # loop para virar a immagem
-	lw $s5,($t0) # carregando os valores das words de forma equidistante em relaÁ„o a horizontal
-	lw $s6,($t1) # carregando os valores das words de forma equidistante em relaÁ„o a horizontal
+	lw $s5,($t0) # carregando os valores das words de forma equidistante em rela√ß√£o a horizontal
+	lw $s6,($t1) # carregando os valores das words de forma equidistante em rela√ß√£o a horizontal
 	sw $s6,($t0) # trocando
 	sw $s5,($t1) # trocando
-	addi $t0,$t0,4 # indo p prÛximo
+	addi $t0,$t0,4 # indo p pr√≥ximo
 	addi $t1,$t1,4 # indo p proximo
 	addi $t3,$t3,1 # contado de largura 
 	beq $t3,$s3,sai2 # ferificando se chegou ao vinda da largura
@@ -91,16 +91,16 @@ sai2:
 	mul $t4,$s3,8 # tamanho em em bytes de dus linha de pixels 
 	sub $t1,$t1,$t4 # subtrai de $t1 que se encontra no final da matriz de pixels
 	addi $t5,$t5,1 # contador 
-	beq $t5,$t2,sai3 # condiÁ„o de parada da funÁ„o de virar o arquivo
+	beq $t5,$t2,sai3 # condi√ß√£o de parada da fun√ß√£o de virar o arquivo
 	j loop2
 sai3:
 ###################################################################################	
 	
-	li $v0, 13
+	li $v0, 16
 	syscall
 	jr $ra
 	
-	read_file: #Funcao ler arquivo que recebe como argumento em $a0 o descritor do arquivo, em $a1 o endere√ßo do buffer 
+	read_file: #Funcao ler arquivo que recebe como argumento em $a0 o descritor do arquivo, em $a1 o endere√É¬ßo do buffer 
 	# na memoria e em $a2 o numero de bytes a serem lidos e retorna em $v0 o numero de bytes lidos ou 0 caso final do arquivo.
 	
 	li $v0, 14
